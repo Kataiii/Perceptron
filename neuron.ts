09e0c409fda4bf6ff3f4f7b4d5b686cadbbf2fb0
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-02-10 21:54:48 +0400

    add class layer

--- neuron.ts
+++ neuron.ts
@@ -1,5 +1,18 @@
 class Neuron {
-    value: number
+    value: number;
+    arrayWeigth : number[];
+
+    adder = (previousLayer: Layer) : number => {
+        let sum : number = 0;
+        for(let i : number = 0; i < previousLayer.arrayNeurons.length; i++){
+            sum = sum + previousLayer.arrayNeurons[i] * this.arrayWeigth[i];
+        }
+        return sum;
+    }
+
+    activationFunction = (previousLayer: Layer) => {
+        this.value = 1 / (1 + Math.pow(Math.E, -this.adder(previousLayer)));
+    }
 }
 
 export default Neuron;
