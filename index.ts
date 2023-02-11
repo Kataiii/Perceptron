94b6ee51f9dad04c99316aeabef6b56f5a3dc227
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-02-11 12:54:52 +0400

    add .gitignore, create constructor perceptron

---
+++ index.ts
@@ -0,0 +1,24 @@
+import Perceptron from "./perceptron";
+
+let perceptron : Perceptron = new Perceptron([11, 9, 7, 5]);
+console.log("--------Perseptron created---------");
+console.log("Neurons:\n");
+for(let i : number = 0; i < perceptron.arrayLayers.length; i++){
+    let str : string = "";
+    for(let j : number = 0; j < perceptron.arrayLayers[i].arrayNeurons.length; j++){
+        str += perceptron.arrayLayers[i].arrayNeurons[j].value + " ";
+        //console.log(perceptron.arrayLayers[i].arrayNeurons[j].value);
+    }
+    console.log(str + "\n");
+}
+
+perceptron.countingHiddenLayers();
+console.log("Neurons:\n");
+for(let i : number = 0; i < perceptron.arrayLayers.length; i++){
+    let str : string = "";
+    for(let j : number = 0; j < perceptron.arrayLayers[i].arrayNeurons.length; j++){
+        str += perceptron.arrayLayers[i].arrayNeurons[j].value + " ";
+        //console.log(perceptron.arrayLayers[i].arrayNeurons[j].value);
+    }
+    console.log(str + "\n");
+}
--- perceptron.ts
+++ perceptron.ts
@@ -6,19 +6,31 @@ class Perceptron{
     arrayLayers : Layer[];
     arrayWeigths : Weigth[];
 
-    constructor(){
-        
+    //TODO дописать конструктор 
+    constructor(countNeuronsInLayers : number[]){
+        for(let i : number = 0; i < countNeuronsInLayers.length; i++){
+            this.arrayLayers.push(new Layer(i));
+        }
+
+        for(let i : number = 0; i < countNeuronsInLayers.length -1; i++){
+            this.arrayWeigths.push(new Weigth(this.arrayLayers[i].arrayNeurons.length, 
+                                                this.arrayLayers[i + 1].arrayNeurons.length));
+        }
     }
 
-    countingIncommingLayer = () => {
+    // countingIncommingLayer = () => {
 
-    }
+    // }
 
     countingHiddenLayers = () => {
-        
+        for(let i : number = 1; i < this.arrayLayers.length; i++){
+            this.arrayLayers[i].countingNeuronsInLayer(this.arrayLayers[i-1],this.arrayWeigths[i]);
+        }
     }
 
-    countingExitingLayer = () => {
+    // countingExitingLayer = () => {
 
-    }
+    // }
 }
+
+export default Perceptron;
