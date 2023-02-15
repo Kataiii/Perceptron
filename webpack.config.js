ed55a9977f5765adcb7b999357732b020fa62bda
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-02-15 08:29:10 +0400

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle-[hash].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  devServer: {
+    contentBase: 'dist',
+    compress: true,
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      }
+    ],
+  }
+};
