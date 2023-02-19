1c20e1d10fea092119a16d8a03481e8b89dc5982
Kataiii <99545244+Kataiii@users.noreply.github.com>
2023-02-19 22:39:10 +0400

    fixed arrays weigths in perceptron

--- src/weigth.ts
+++ src/weigth.ts
@@ -3,13 +3,14 @@ class Weigth{
     arrayWeigth : number[][];
 
     constructor(previousLayerLength: number, currentLayerLength : number){
+        console.log('Weigth : '+ previousLayerLength + " " + currentLayerLength);
         this.arrayWeigth = new Array<Array<number>>(previousLayerLength);
-        for(let i : number = 0; i < currentLayerLength; i++){
+        for(let i : number = 0; i < previousLayerLength; i++){
             this.arrayWeigth[i] = new Array<number>(currentLayerLength);
         }
 
         for(let i : number = 0; i < previousLayerLength; i++){
-            for(let j : number = 0; i < currentLayerLength; i++){
+            for(let j : number = 0; j < currentLayerLength; j++){
                 this.arrayWeigth[i][j] = Math.floor(Math.random() * 101)/ 100;
             }
         }
